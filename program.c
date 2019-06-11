b684892b082c347a7412b62577dbffc5b1883429
tajpouria <charliswager@gmail.com>
2019-06-11 23:55:09 +0430

    session nine added

--- program.c
+++ program.c
@@ -1,5 +1,105 @@
 #include <stdio.h>
 
+// session nine
+
+void nine_p16_1_findMaximum(int array[3])
+{
+  int max = array[0];
+
+  for (int i = 1; i <= 2; i++)
+    (max < array[i]) ? max = array[i] : 0;
+
+  printf("%d \n", max);
+}
+
+void nine_p16()
+{
+  int inputs[3];
+
+  for (int i = 0; i < 3; i++)
+  {
+    printf("Please Enter value number #%d : ", i + 1);
+    scanf("%d", &inputs[i]);
+  }
+
+  nine_p16_1_findMaximum(inputs);
+}
+
+void nine_p18_1_conver(int array[3])
+{
+  printf("%d \n", 3600 * array[0] + 60 * array[1] + array[2]);
+}
+
+void nine_p18()
+{
+  char *utils[] = {"Hour", "Minute", "Second"};
+  int getUtils[3];
+
+  for (int i = 0; i < 3; i++)
+  {
+    printf("Please enter %s : ", utils[i]);
+    scanf("%d", &getUtils[i]);
+  }
+
+  nine_p18_1_conver(getUtils);
+}
+
+float p_21_1_calculateRadius(float radius)
+{
+  return radius * radius * 3.14;
+}
+
+void nine_p21()
+{
+  float radius;
+
+  printf("Please enter radius of Circle : ");
+  scanf("%f", &radius);
+
+  float Area = p_21_1_calculateRadius(radius);
+
+  printf("Area is equals %f \n", Area);
+}
+
+int nine_p22_1_isPrime(int number)
+{
+  for (int i = 2; i < number; i++)
+    if (number % i == 0)
+      return 0;
+
+  return 1;
+}
+
+void nine_p22()
+{
+  int number;
+
+  printf("Please enter your number : ");
+  scanf("%d", &number);
+
+  nine_p22_1_isPrime(number) ? printf("Given number is prime \n") : printf("Given number is not prime \n");
+}
+
+int nine_p29_1_fact(int number)
+{
+  if (number == 0)
+    return 1;
+
+  return (number * nine_p29_1_fact(number - 1));
+}
+
+void nine_29()
+{
+  int number;
+
+  printf("Please enter number for calculating it's fact : ");
+  scanf("%d", &number);
+
+  int fact = nine_p29_1_fact(number);
+
+  printf("%d! = %d\n", number, fact);
+}
+
 // session ten
 
 void ten_p7()
@@ -99,6 +199,15 @@ void ten_p15()
 
 int main()
 {
+
+  // session nine
+
+  // nine_p16();
+  // nine_p18();
+  // nine_p21();
+  // nine_p22();
+  // nine_29();
+
   //session ten
 
   // ten_p7();
